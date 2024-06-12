58a9918236e67f0a6557f9c647f6f176cc0fd5c6
VadimSorokolit <macintosh@ukr.net>
2024-06-12 20:37:26 +0300

    Added xcodeproj v3

---
+++ ContactsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by Vadym Sorokolit on 05.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
