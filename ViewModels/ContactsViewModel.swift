227e08f1afa342a768d1d7639f0b2dcd7cd8aecd
VadimSorokolit <macintosh@ukr.net>
2024-06-20 17:56:49 +0300

    ContactsViewModel: Implemented notifiers  changing through NotificationCenter

--- ViewModels/ContactsViewModel.swift
+++ ViewModels/ContactsViewModel.swift
@@ -17,26 +17,44 @@ class ContactsViewModel {
     // MARK: - Methods
     
     func fetchContacts() -> [Contact] {
-        return self.coreDataService.fetchContacts()
+        let contacts = self.coreDataService.fetchContacts()
+        NotificationCenter.default.post(name: .contactsFetchedNotification, object: nil)
+        return contacts
     }
     
     func fetchContact(byEmail email: String) -> Contact? {
-        return self.coreDataService.fetchContact(byEmail: email)
+        let contact = self.coreDataService.fetchContact(byEmail: email)
+        if contact != nil {
+            NotificationCenter.default.post(name: .contactFetchedNotification, object: nil)
+        }
+        return contact
     }
     
     private func createContact(fullName: String, jobPosition: String, email: String, photo: UIImage?) {
         self.coreDataService.createContact(fullName: fullName, jobPosition: jobPosition, email: email, photo: photo)
+        NotificationCenter.default.post(name: .contactCreatedNotification, object: nil)
     }
     
     private func updateContact(byEmail email: String, jobPostion: String) {
         self.coreDataService.updateContact(byEmail: email, jobPosition: jobPostion)
+        NotificationCenter.default.post(name: .contactUpdatedNotification, object: nil)
     }
     
     private func deleteContact(byEmail email: String) {
         self.coreDataService.deleteContact(byEmail: email)
+        NotificationCenter.default.post(name: .contactDeletedNotification, object: nil)
     }
     
 }
 
+extension Notification.Name {
+    
+    static let contactsFetchedNotification = Notification.Name("contactsFetchedNotification")
+    static let contactFetchedNotification = Notification.Name("contactFetchedNotification")
+    static let contactCreatedNotification = Notification.Name("contactCreatedNotification")
+    static let contactUpdatedNotification = Notification.Name("contactUpdatedNotification")
+    static let contactDeletedNotification = Notification.Name("contactDeletedNotification")
+    
+}
 
 
