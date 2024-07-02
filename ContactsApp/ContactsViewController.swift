eb726a8db77ce140612968bc5cde7f2e490c03ef
VadimSorokolit <macintosh@ukr.net>
2024-07-02 18:51:42 +0300

    CocntactsViewController: Added title to navigation bar and searchController

--- ContactsApp/ContactsViewController.swift
+++ ContactsApp/ContactsViewController.swift
@@ -7,28 +7,73 @@
 
 import UIKit
 
-class ContactsViewController: UIViewController {
+class ContactsViewController: UIViewController, UISearchResultsUpdating {
+    
+    lazy var searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        searchController.hidesNavigationBarDuringPresentation = false
+        return searchController
+    }()
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.setup()
+        setup()
     }
     
     // MARK: - Methods
     
     private func setup() {
         self.setupNavBar()
+        self.setupSearchController()
         self.setupViews()
     }
     
     private func setupNavBar() {
+        self.navigationItem.largeTitleDisplayMode = .always
+        
+        let titleLabel = UILabel()
+        titleLabel.text = NSLocalizedString("Contacts", comment: "")
+        titleLabel.textAlignment = .left
+        titleLabel.font = UIFont.systemFont(ofSize: 28, weight: .bold)
+        
+        let titleContainerView = UIView()
+        titleContainerView.addSubview(titleLabel)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: titleContainerView.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: titleContainerView.trailingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: titleContainerView.topAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: titleContainerView.bottomAnchor),
+            titleContainerView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width - 30)
+        ])
+        
+        self.navigationItem.titleView = titleContainerView
     }
     
     private func setupViews() {
         self.view.backgroundColor = .gray
+    }
+    
+    private func setupSearchController() {
+        navigationItem.searchController = self.searchController
+        definesPresentationContext = true
+        
+        self.searchController.searchBar.showsCancelButton = false
+    }
+    
+    // MARK: - UISearchResultsUpdating
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text, !searchText.isEmpty {
+            print(searchText)
+        }
     }
     
 }
