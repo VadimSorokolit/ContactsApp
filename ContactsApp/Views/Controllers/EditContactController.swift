5aab25cf9249636b51d9398684d71253e7f9adc1
VadimSorokolit <macintosh@ukr.net>
2024-07-16 12:01:09 +0300

    EditContactController:  Created new class EditContactController:

---
+++ ContactsApp/Views/Controllers/EditContactController.swift
@@ -0,0 +1,11 @@
+//
+//  EditContactController.swift
+//  ContactsApp
+//
+//  Created by Vadim Sorokoliton 16.07.2024.
+//
+
+import Foundation
+import UIKit
+
+class EditContactController: UIViewController {}
