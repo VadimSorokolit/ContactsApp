91602580c2473e31e0c369d3b5b0e3745ba66d9d
VadimSorokolit <macintosh@ukr.net>
2024-08-12 23:33:41 +0300

    SceneDelegate: Code refactoring

--- ContactsApp/SupportingFiles/SceneDelegate.swift
+++ ContactsApp/SupportingFiles/SceneDelegate.swift
@@ -10,13 +10,23 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
+    // MARK: - Properties
+    
     var window: UIWindow?
+    let service: IAPIContacts
+    
+    // MARK: - Initializer
+    
+    init(service: IAPIContacts) {
+        self.service = service
+    }
+    
+    // MARK: - Methods
  
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let apiContacts: IAPIContacts = CoreDataService()
-        let contactsViewModel = ContactsViewModel(service: apiContacts)
+        let contactsViewModel = ContactsViewModel(service: self.service)
         let bootViewController = ContactsViewController(contactsViewModel: contactsViewModel)
         
         let window = UIWindow(windowScene: windowScene)
